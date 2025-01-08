74e446a0ec19013c79bcb3fc68675b6aa6975ac4
JeremiahK <jeremiah.kriegel@gmail.com>
2025-01-07 21:14:12 -0600

    Secuirty Update

--- resources/js/layouts/GuestLayout.vue
+++ resources/js/layouts/GuestLayout.vue
@@ -8,7 +8,7 @@ import { Link } from '@inertiajs/vue3';
         <div>
             <h1 class="text-dark dark:text-white font-bold text-2xl">FULLSTACKJEM</h1>
         </div>
-
+        staging.jmkweb.us
         <div
             class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg"
         >
